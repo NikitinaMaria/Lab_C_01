b169a739bc7499c9186f99b9e8e982dc3e092a36
NikitinaMaria <71255897+NikitinaMaria@users.noreply.github.com>
2021-02-04 10:36:18 +0300

    Упражнение 2

--- zadanie_2.cpp
+++ zadanie_2.cpp
@@ -7,9 +7,10 @@ int main()
 {
     cout << "Enter the length of the first leg:" << endl;
     int x;
+    cin >> x
     cout << "Enter Enter the length of the second leg:" << endl;
     int y;
-    cin >> x >> y;
+    cin >> y;
     cout << sqrt(x*x + y*y) << endl;
     return 0;
 }
