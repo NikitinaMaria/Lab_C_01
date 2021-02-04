fe821dfbca2fadefce763e92f585c2c0c680199d
NikitinaMaria <71255897+NikitinaMaria@users.noreply.github.com>
2021-02-04 10:38:54 +0300

    Упражнение 2

--- zadanie_2.cpp
+++ zadanie_2.cpp
@@ -7,10 +7,10 @@ int main()
 {
     cout << "Enter the length of the first leg:" << endl;
     int x;
-    cin >> x
+    cin >> x;
     cout << "Enter Enter the length of the second leg:" << endl;
     int y;
     cin >> y;
-    cout << sqrt(x*x + y*y) << endl;
+    cout << "The length of the hypotenuse: " << sqrt(x * x + y * y) << endl;
     return 0;
 }
