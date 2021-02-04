a21833f20948d2ca229d7b5917333c6d21035f6a
NikitinaMaria <71255897+NikitinaMaria@users.noreply.github.com>
2021-02-04 10:35:32 +0300

    Упражнение 2

--- zadanie_2.cpp
+++ zadanie_2.cpp
@@ -5,8 +5,9 @@ using namespace std;
 
 int main()
 {
-    cout << "Enter N:" << endl;
+    cout << "Enter the length of the first leg:" << endl;
     int x;
+    cout << "Enter Enter the length of the second leg:" << endl;
     int y;
     cin >> x >> y;
     cout << sqrt(x*x + y*y) << endl;
