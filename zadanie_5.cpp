98832246c8597ef053f88ef346aa21efe4cb8cd8
NikitinaMaria <71255897+NikitinaMaria@users.noreply.github.com>
2021-02-04 10:32:27 +0300

    Упражнение 5

---
+++ zadanie_5.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    cout << "Enter N:" << endl;
+    int N;
+    cin >> N;
+    for (int i = 1; i <= N; i++) {
+        for (int j = 1; j <= N - i + 1; j++) {
+            cout << '*';
+        }
+        cout << endl;
+    }
+    return 0;
+}
